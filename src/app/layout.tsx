7bb5a2923ff4548b2a4b07a9dbb03c900aa3f824
Abuzar <abuzar0561446@gmail.com>
2024-04-18 12:12:55 +0530

    Next js Project Initialize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,7 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
+      <div className="flex justify-center items-center relative w-full">
+      <Navbar/>
+      </div>
       <body className={inter.className}>{children}</body>
     </html>
   );
