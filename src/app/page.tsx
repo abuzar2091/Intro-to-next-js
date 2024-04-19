d6a191b5efffa6c5e3021b31022b8b4fb3462d6f
Abuzar <abuzar0561446@gmail.com>
2024-04-19 20:54:30 +0530

    Implement infinite moving card ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-
-
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChoseUs from "@/components/WhyChoseUs";
 export default function Home() {
   return (
     <main className=" min-h-screen text-white bg-black/[.9] text-center antialiased">
-    
      <div>
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChoseUs/>
+      <TestimonialCards/>
      </div>
    
     </main>
